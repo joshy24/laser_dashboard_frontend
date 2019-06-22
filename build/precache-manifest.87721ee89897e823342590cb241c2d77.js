61d3e056fa3194997a18c44671f767706dfc78a9
josh <joshuamajebi@yahoo.com>
2019-06-22 11:30:22 +0100

    msg

---
+++ build/precache-manifest.87721ee89897e823342590cb241c2d77.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55fb7d5e3e7dc27310f46082e23f73c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "417a5201b837d79a46d2",
+    "url": "/static/css/2.2507d4ca.chunk.css"
+  },
+  {
+    "revision": "034803aba6cb23f68f1b",
+    "url": "/static/css/main.110dca53.chunk.css"
+  },
+  {
+    "revision": "417a5201b837d79a46d2",
+    "url": "/static/js/2.e8dce6a3.chunk.js"
+  },
+  {
+    "revision": "034803aba6cb23f68f1b",
+    "url": "/static/js/main.8712ff16.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f7505e8c069c019627155501a1efe47",
+    "url": "/static/media/call.8f7505e8.png"
+  },
+  {
+    "revision": "be8584e224b6281f73725167e4f7d0cd",
+    "url": "/static/media/call_with_circle.be8584e2.png"
+  },
+  {
+    "revision": "5372e70f414ad1908a74a37ae392c76e",
+    "url": "/static/media/emergency.5372e70f.png"
+  },
+  {
+    "revision": "d3c347cfccc4ac855e2682bacfed09f7",
+    "url": "/static/media/emergency_with_circle.d3c347cf.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
