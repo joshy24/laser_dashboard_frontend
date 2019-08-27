ba7c66d5bfc4db45c22a4cf7719935c60623ed2c
josh <joshuamajebi@yahoo.com>
2019-08-27 15:24:51 +0100

    production setup

--- config/config.js
+++ config/config.js
@@ -7,11 +7,11 @@ module.exports = {
     port: process.env.PORT,
     base_url: process.env.BASE_URL,
     db: {
-        uri: process.env.DB_URL/*,
+        uri: process.env.DB_URL,
         options: {
             user: process.env.DB_USER,
             pass: process.env.DB_PWD
-        }*/
+        }
     },
     secret: process.env.SECRET_KEY, 
     socket: {
