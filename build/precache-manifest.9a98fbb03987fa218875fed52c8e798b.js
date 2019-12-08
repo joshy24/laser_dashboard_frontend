8ebe5a01850e43c3ebc9b2eaebc60e123bc2499d
josh <joshuamajebi@yahoo.com>
2019-12-08 10:02:40 +0100

    message

---
+++ build/precache-manifest.9a98fbb03987fa218875fed52c8e798b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5c00a9d45d272994e11eb09a3dc9e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "522ccaaee8d2aedf348f",
+    "url": "/static/css/2.2507d4ca.chunk.css"
+  },
+  {
+    "revision": "ed411733045b97d2c39e",
+    "url": "/static/css/main.874eaba5.chunk.css"
+  },
+  {
+    "revision": "522ccaaee8d2aedf348f",
+    "url": "/static/js/2.d5f180cc.chunk.js"
+  },
+  {
+    "revision": "ed411733045b97d2c39e",
+    "url": "/static/js/main.9c7115c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "77e4ae16960a50687d21cbf660a767e3",
+    "url": "/static/media/alert.77e4ae16.mp3"
+  },
+  {
+    "revision": "3ff333a4002b262173fd64b9d5889118",
+    "url": "/static/media/ambulance.3ff333a4.png"
+  },
+  {
+    "revision": "8b74e8df00c57ca7eed80bf2afa06597",
+    "url": "/static/media/ambulance_yellow.8b74e8df.png"
+  },
+  {
+    "revision": "bb9cbc9b392a2444cbb3fa91a91b5542",
+    "url": "/static/media/bg.bb9cbc9b.jpg"
+  },
+  {
+    "revision": "f128647bce69fad97362bf4147ab95d8",
+    "url": "/static/media/burger_menu.f128647b.png"
+  },
+  {
+    "revision": "37865c675678954560bc6a08299c69a7",
+    "url": "/static/media/call.37865c67.gif"
+  },
+  {
+    "revision": "34c874787b0237498882ff0d8d3999d1",
+    "url": "/static/media/call_with_circle.34c87478.gif"
+  },
+  {
+    "revision": "4b63a07a0882067e4f935d9e6fd4f4e8",
+    "url": "/static/media/car_test.4b63a07a.png"
+  },
+  {
+    "revision": "445ccffba67c778b2d4e50d6e9529661",
+    "url": "/static/media/emergency.445ccffb.gif"
+  },
+  {
+    "revision": "0153b7ea9c6a8e8e85c61c5d336d0106",
+    "url": "/static/media/emergency_with_circle.0153b7ea.gif"
+  },
+  {
+    "revision": "651626d2eba1ecd9e6c9a2a2dd2f1099",
+    "url": "/static/media/fire_truck.651626d2.png"
+  },
+  {
+    "revision": "4a7d405136199c9f39294fbe9871ef63",
+    "url": "/static/media/fire_truck_yellow.4a7d4051.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "23d1feaf1ceefd9a550e0de23960cb50",
+    "url": "/static/media/phone_white.23d1feaf.png"
+  },
+  {
+    "revision": "d5d82e204eb143fe019d0143f4a65651",
+    "url": "/static/media/police_car.d5d82e20.png"
+  },
+  {
+    "revision": "303f78975daae26f5a411536a4f2b2d6",
+    "url": "/static/media/police_car_yellow.303f7897.png"
+  }
+]);
